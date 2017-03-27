8cbfa8929bac2e91448914bd0bbabecf1be44d3e
Danilo Vukomanovic <danilov@tabtale.com>
2017-03-27 17:29:27 +0200

    refactor

--- ButtonView/ButtonLoadMenuView.cs
+++ ButtonView/ButtonLoadMenuView.cs
@@ -3,17 +3,16 @@ using System.Collections;
 
 namespace PofyTools
 {
-	public class ButtonLoadMenuView : ButtonView
-	{
-		#region implemented abstract members of ButtonView
+    public class ButtonLoadMenuView : ButtonView
+    {
+        #region implemented abstract members of ButtonView
 
-		protected override void OnClick ()
-		{
-			LevelLoader.LevelLoader.LoadMenu ();
-		}
+        protected override void OnClick()
+        {
+            LevelLoader.LevelLoader.LoadMenu();
+        }
 
-		#endregion
+        #endregion
 
-
-	}
+    }
 }
