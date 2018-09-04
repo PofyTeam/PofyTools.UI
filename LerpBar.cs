a8725cff03fd33ff57a1774b403645d2f2034428
Danilo Čoda Vukomanović <dvukomanovic@gmail.com>
2018-09-05 01:05:05 +0200

    switched to use sliced bars size instead of fill amount

--- LerpBar.cs
+++ LerpBar.cs
@@ -6,11 +6,27 @@ namespace PofyTools.UI
     public class LerpBar : Panel
     {
 
+        #region IInitializable
+
+        public override bool Initialize()
+        {
+            if (base.Initialize())
+            {
+                this._barWidthRange = new Range(0, this._frame.rectTransform.sizeDelta.x);
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+
         [SerializeField]
         protected AnimationCurve _lerpCurve;
 
         [SerializeField]
         protected Image _frame, _fill, _fillLerp;
+
+        private Range _barWidthRange;
 
         private float _currnetValue;
 
@@ -21,11 +37,15 @@ namespace PofyTools.UI
         public void SetLerpValue(float normalizedValue)
         {
 
-
             if (normalizedValue < this._currnetValue)
             {
-                this._fill.fillAmount = normalizedValue;
+                //this._fill.fillAmount = normalizedValue;
+                Vector2 size = this._fill.rectTransform.sizeDelta;
+                size.x = this._barWidthRange.MappedPoint(normalizedValue);
+                this._fill.rectTransform.sizeDelta = size;
+
                 this._currnetValue = normalizedValue;
+
                 LerpDown();
             }
             else
@@ -39,8 +59,14 @@ namespace PofyTools.UI
         public void SetValue(float normalizedValue)
         {
             this._currnetValue = normalizedValue;
-            this._fill.fillAmount = normalizedValue;
-            this._fillLerp.fillAmount = normalizedValue;
+            Vector2 size = this._fill.rectTransform.sizeDelta;
+            size.x = this._barWidthRange.MappedPoint(normalizedValue);
+
+            this._fill.rectTransform.sizeDelta = size;
+            this._fillLerp.rectTransform.sizeDelta = size;
+
+            //this._fill.fillAmount = normalizedValue;
+            //this._fillLerp.fillAmount = normalizedValue;
 
         }
 
@@ -54,7 +80,8 @@ namespace PofyTools.UI
         private void LerpUp()
         {
             //if (!this._lerpUpState.isActive)
-            AddState(this._lerpUpState);
+            //AddState(this._lerpUpState);
+            SetValue(this._currnetValue);
         }
 
         #endregion
@@ -73,7 +100,7 @@ namespace PofyTools.UI
 
         public class LerpDownState : TimedStateObject<LerpBar>
         {
-            private float _startValue;
+            private Vector2 _startValue;
 
             public LerpDownState(LerpBar controlledObject, Range timeRange, AnimationCurve curve) : base(controlledObject, timeRange, curve)
             {
@@ -83,7 +110,8 @@ namespace PofyTools.UI
             public override void EnterState()
             {
                 base.EnterState();
-                this._startValue = this[0]._fillLerp.fillAmount;
+                //this._startValue = this[0]._fillLerp.fillAmount;
+                this._startValue = this[0]._fillLerp.rectTransform.sizeDelta;
 
                 this._timeRange.Current = 0f;
             }
@@ -93,7 +121,8 @@ namespace PofyTools.UI
                 this._timeRange.Current += Time.unscaledDeltaTime;
 
                 var time = this._curve.Evaluate(this._timeRange.CurrentToMaxRatio);
-                this[0]._fillLerp.fillAmount = Mathf.Lerp(this._startValue, this[0]._fill.fillAmount, time);
+
+                this[0]._fillLerp.rectTransform.sizeDelta = Vector2.Lerp(this._startValue, this[0]._fill.rectTransform.sizeDelta, time);
 
                 if (this._timeRange.AtMax)
                     return true;
